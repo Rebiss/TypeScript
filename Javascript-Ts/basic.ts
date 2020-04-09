eb8893639e8628ce4a121eba80991ace88f0ade1
Andranik Barseghyan <barsegyanat.93@gmail.com>
2020-04-09 23:10:42 +0400

    add: Basic type

---
+++ Javascript-Ts/basic.ts
@@ -0,0 +1,23 @@
+/**
+ * ! Data Types in JS
+ * ! Number, String, Boolean, null, underfind, Object, Symbol
+ * !typeof operator
+ */
+
+let isBool: boolean = "true";
+isBool = 42;
+isBool = false;
+
+let isNum: number = 1256;
+let int: number = 9.366555;
+
+let isStr: string = "string";
+
+const isUnder: undefined = undefined; //typeof underfined( JS=> object)
+const isNull: null = null; //typeof object
+
+// Void
+
+const setFunc = (): void => {
+  alert("Alelua");
+}; // Error no return
